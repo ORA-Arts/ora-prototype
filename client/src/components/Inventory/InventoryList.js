fbfaade824acb011c9a8c7510a00b553470887ae
honghainguyen777 <n.h.hai2015@gmail.com>
2021-03-07 10:26:56 +0100

    layout artwork list - done!

--- client/src/components/Inventory/InventoryList.js
+++ client/src/components/Inventory/InventoryList.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import "./Inventory.css";
 import ProfileSideBar from "../ProfileSideBar/ProfileSideBar";
-// import house from './house-test.jpg';
+import house from './house-test.jpg';
 import axios from 'axios';
 import { fetchGallery, addNewGallery } from '../../api/service';
 
@@ -98,122 +98,62 @@ const InventoryList = (props) => {
                     <label className="label-button-like" htmlFor="sort-by-value">SORT BY VALUE</label>
                 </div>
             </div>
-            <hr/>
-            <div className="artwork-list"></div>
+            {/* <hr/> */}
+            <div className="artwork-list">
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
+                <div className="artwork-list-item">
+                    <img src={house} alt="test-image" className="artwork-list-item-thumbnail" />
+                    <div className="artwork-list-item-info"><span className="artwork-list-item-name">Brigit Megerle</span>, Title & Year, Signed, dated and numbered, Material, Dimension, Stock number, Price, Location</div>
+                </div>
 
-
-
-
-            <div className="profile-title">
-              <hr />
-              <span className="title-text">
-                PERSONAL INFORMATION
-              </span>
             </div>
-            <div className="input-block">
-              {/* replace input by real information later */}
-              <div className="input-container-half">
-                  <span className="input-label input-lable-right">USER NAME/ </span>
-                  {/* <input type="text" name="username" onChange={e => setUsername(e.target.value.toUpperCase())} value={username} className="input-no-border" placeholder="THOMAS BALLOT" role="textbox"></input> */}
-                  {isEditMode ? 
-                    <input type="text" name="ownerName" onChange={onChange} value={data.ownerName} className="input-no-border" placeholder="THOMAS BALLOT" role="textbox"></input> :
-                    <span>{data.ownerName}</span>
-                  }
-              </div>
-              <div className="input-container-half">
-                  <span className="input-label input-lable-right">EMAIL/ </span>
-                  {isEditMode ? 
-                    <input type="email" name="email" onChange={onChange} value={data.email} className="input-no-border" placeholder="THIBAULT.HENRIET@ORA-ARTS.COM"></input> :
-                    <span>{data.email}</span>
-                  }
-              </div>
-              <div className="input-container-half">
-                  {/* should not display even in edit */}
-                  <span className="input-label input-lable-right">PASSWORD/ </span>
-                  {isEditMode ? 
-                    <input type="password" name="password" onChange={onChange} value={data.password} className="input-no-border" placeholder="Password"></input> :
-                    <span>{data.password}</span>
-                  }
-              </div>
-              <div className="input-container-half">
-                  <span className="input-label input-lable-right">GALLERY POSITION/ </span>
-                  {isEditMode ? 
-                    <input type="text" name="position" onChange={onChange} value={data.position} className="input-no-border" placeholder="Director"></input> :
-                    <span>{data.position}</span>
-                  }
-              </div>
-            </div>
-            <div className="profile-title space-top">
-              <hr />
-              <span className="title-text">
-                PRACTICAL INFORMATION
-              </span>
-            </div>
-            <div className="input-block">
-              <div className="input-container-full">
-                  <span className="input-label">NAME OF THE GALLERY/ </span>
-                  {isEditMode ? 
-                    <input type="text" name="name" onChange={onChange} value={data.name} className="input-no-border" placeholder="GALLERY NEU"></input> :
-                    <span>{data.name}</span>
-                  }
-              </div>
-              <div className="input-container-full">
-                  <span className="input-label">ADDRESS/ </span>
-                  {isEditMode ? 
-                    <input type="text" name="address" onChange={onChange} value={data.address} className="input-no-border" placeholder="435 Broadway New York 10013 Manhattan"></input> :
-                    <span>{data.address}</span>
-                  } 
-              </div>
-            </div>
-            <div className="input-block large-box">
-              <div className="input-container-half">
-                  <div className="biography-box">
-                      <div className="input-label input-lable-left">GALLERY BIOGRAPHY</div>
-                      <div>
-                        {isEditMode ? 
-                          <textarea name="biography" onChange={onChange} value={data.biography} id="biography" className="input-no-border" rows="24" placeholder="your gallery biography"></textarea> :
-                        <div className="biography-text">{data.biography}</div>
-                        } 
-                      </div>
-                  </div>
-              </div>
-              <div className="input-container-half remove-border">
-                  <div className="image-container">
-                      <img className="gallery-image" src={image ? URL.createObjectURL(image) : data.imageUrl} alt={image ? image.name.split(".")[0] : "house sample"} />
-                      <input type={(isGalleryExist && !isEditMode) ? "hidden" : "file"} id="file" className="input-hidden" onChange={fileHandler}  />
-                      <label htmlFor="file" className="btn-image">CHANGE IMAGE</label>
-                      {/* <button className="btn-image">CHANGE IMAGE</button> */}
-                  </div>
-                  <div className="input-container-half">
-                      <div className="website-box">
-                          <div className="input-label input-lable-left">WEBSITE </div>
-                          <div className="input-lable-left">
-                              {isEditMode ? 
-                                <input type="text" name="website" onChange={onChange} value={data.website} className="input-no-border" placeholder="www.gallerieneu.com"></input> :
-                                <span>{data.website}</span>
-                              } 
-                          </div>
-                      </div>
-                  </div>
-              </div>
-            </div>
-            <div className="profile-title space-top">
-              <hr />
-              <span className="title-text">
-                SHIPPING INFORMATION
-              </span>
-            </div>
-            <div className="shipping-info">
-              <span className="shipping-title">CONVELIO</span>
-              <div id="selectTopic">
-              {/* checked={this.state.convelio === true} */}
-                  <input type="radio" id="yes" name="convelio" onChange={e => setConvelio(true)} checked={convelio} />
-                  <label htmlFor="yes">YES</label>
-                  <input type="radio" id="no" name="convelio" onChange={e => setConvelio(false)} checked={!convelio}/>
-                  <label htmlFor="no">NO</label>
-              </div>
-            </div>
-            <button className="btn-edit save-change space-top" onClick={submitHandler}>SAVE CHANGES</button>
           </div>
         </div>
       </div>
