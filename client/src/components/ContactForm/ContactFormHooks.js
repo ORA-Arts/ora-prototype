a8c30c2a7c2f6487b3f703b18dc98348c0416433
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-03-09 20:51:46 +0100

    done contact form responsivines

--- client/src/components/ContactForm/ContactFormHooks.js
+++ client/src/components/ContactForm/ContactFormHooks.js
@@ -36,9 +36,9 @@ export default function ContactFormHooks(props) {
             .catch((err) => {
                 console.log('Message not sent', err)
             })
-            setSent(true)
-            console.log('sent: ',sent)
-            resetForm()
+        setSent(true)
+        console.log('sent: ', sent)
+        resetForm()
     }
 
     const resetForm = () => {
@@ -46,95 +46,92 @@ export default function ContactFormHooks(props) {
     }
 
     return (
-        <div className='contact-us'>
-            <div className="tabsContact">
-                <div id="contactContainer" className="animate__animated animate__fadeInUp">
-                    <div id="contactImage">
+        <div className="contactContainer">
+            <div className="contactImage">
+            </div>
+            <div className="formContainer">
+                <Tabs className="tabContact">
+                    <div className='contactHeader'>
+                    <TabList>
+                        <Tab>CONTACT US</Tab> <span>/</span> <Tab>TEAM</Tab>
+                    </TabList>
                     </div>
-                    <div id="formContainer">
-                        <Tabs className="tabContact">
-                            <TabList>
-                                <Tab>CONTACT US</Tab> <span>/</span> <Tab>TEAM</Tab>
-                            </TabList>
-                            <TabPanel>
-                                <h2>GET IN TOUCH WITH OUR TEAM REGARDING ANY QUESTIONS YOU MIGHT HAVE WITH OUR ACTIVITIES</h2>
-                                <form id="contactForm" onSubmit={submitEmail} method='POST'>
-                                    <div id="emailAndName">
-                                        <input type="text" name="name" value={data.name} onChange={handleInputChange} placeholder="NAME*" required />
-                                        <input type="email" name="email" value={data.email} onChange={handleInputChange} placeholder="EMAIL*" required />
+                    <TabPanel>
+                        <h2>GET IN TOUCH WITH OUR TEAM REGARDING ANY QUESTIONS YOU MIGHT HAVE WITH OUR ACTIVITIES</h2>
+                        <form id="contactForm" onSubmit={submitEmail} method='POST'>
+                            <div id="emailAndName">
+                                <input type="text" name="name" value={data.name} onChange={handleInputChange} placeholder="NAME" required />
+                                <input type="email" name="email" value={data.email} onChange={handleInputChange} placeholder="EMAIL" required />
+                            </div>
+                            <div id="topicContainer">
+                                <p>TOPIC</p>
+                                <div className='radio-toolbar'>
+                                    <div>
+                                        <input onChange={onChange} id="radioSupport" type="radio" value="SUPPORT AN ARTIST PROJECT"
+                                            checked={(checkedOption === 'SUPPORT AN ARTIST PROJECT')}></input>
+                                        <label htmlFor='radioSupport'>SUPPORT AN ARTIST PROJECT</label>
                                     </div>
-                                    <div id="topicContainer">
-                                        <p>TOPIC*</p>
-                                        <div className='radio-toolbar'>
-                                            <div>
-                                                <input onChange={onChange} id="radioSupport" type="radio" value="SUPPORT AN ARTIST PROJECT"
-                                                    checked={(checkedOption === 'SUPPORT AN ARTIST PROJECT')}></input>
-                                                <label htmlFor='radioSupport'>SUPPORT AN ARTIST PROJECT</label>
-                                            </div>
 
-                                            <div>
-                                                <input onChange={onChange} id="radioPersonal" type="radio" value="PERSONAL ADVISOR"
-                                                    checked={(checkedOption === 'PERSONAL ADVISOR')}></input>
-                                                <label htmlFor='radioPersonal'>PERSONAL ADVISOR</label>
-                                            </div>
+                                    <div>
+                                        <input onChange={onChange} id="radioPersonal" type="radio" value="PERSONAL ADVISOR"
+                                            checked={(checkedOption === 'PERSONAL ADVISOR')}></input>
+                                        <label htmlFor='radioPersonal'>PERSONAL ADVISOR</label>
+                                    </div>
 
-                                            <div>
-                                                <input onChange={onChange} type="radio" id="radioSpecial" value="SPECIAL SOURCING"
-                                                    checked={(checkedOption === 'SPECIAL SOURCING')}></input>
-                                                <label htmlFor='radioSpecial'>SPECIAL SOURCING</label>
-                                            </div>
+                                    <div>
+                                        <input onChange={onChange} type="radio" id="radioSpecial" value="SPECIAL SOURCING"
+                                            checked={(checkedOption === 'SPECIAL SOURCING')}></input>
+                                        <label htmlFor='radioSpecial'>SPECIAL SOURCING</label>
+                                    </div>
 
-                                            <div>
-                                                <input onChange={onChange} type="radio" id="radioArtist" value="ARTIST OPEN CALL"
-                                                    checked={(checkedOption === 'ARTIST OPEN CALL')}></input>
-                                                <label htmlFor='radioArtist'>ARTIST OPEN CALL</label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div id="inquireAndButton">
-                                        <textarea type="text" name="body" value={data.body} id="body" placeholder="INQUIRE ABOUT*" required rows="25" cols="30" onChange={handleInputChange}> </textarea>
-                                        <div className="contactFooter">
-                                        <div className='sentMessage'>
-                                            {sent ? <span>YOUR MESSAGE HAS BEEN SENT</span> : <span> </span>}
-                                        </div>    
-                                        <Link to='mailto:info@ora-arts.com'><h3 className='contact-email'>INFO@ORA-ARTS.COM</h3></Link>
-                                        <button type="submit" className="btnWhite">SEND MESSAGE</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </TabPanel>
-                            <TabPanel>
-                                <div className="teamTab">
-                                    <div className='teamMember'>
-                                        <h3>THIBAULT HENRIET</h3>
-                                        PRESIDENT AND FOUNDER
-                                    </div>
-                                    <div className='teamMember'>
-                                        <h3>LAURELINE CALASTRENG</h3>
-                                        EXECUTIVE DIRECTOR
-                                    </div>
-                                    <div className='teamMember'>
-                                        <h3>RACHEL LEQUESNE</h3>
-                                        HEAD OF DIFFUSION
-                                    </div>
-                                    <div className='teamMember'>
-                                        <h3>ARTHUR MORISSET</h3>
-                                        ARTISTIC DIRECTOR / HEAD OF PRODUCTION
-                                    </div>
-                                    <div className='teamMember'>
-                                        <h3>CORENTINE PIETTE</h3>
-                                        CTO
-                                    </div>
-                                    <div className='teamMember'>
-                                        <Link to='mailto:info@ora-arts.com'><h3 className='contact-email'>INFO@ORA-ARTS.COM</h3></Link>
+                                    <div>
+                                        <input onChange={onChange} type="radio" id="radioArtist" value="ARTIST OPEN CALL"
+                                            checked={(checkedOption === 'ARTIST OPEN CALL')}></input>
+                                        <label htmlFor='radioArtist'>ARTIST OPEN CALL</label>
                                     </div>
                                 </div>
-                            </TabPanel>
-                        </Tabs>
-                    </div>
-                </div>
+                            </div>
+                            <div id="inquireAndButton">
+                                <textarea type="text" name="body" value={data.body} id="body" placeholder="INQUIRE ABOUT*" required rows="25" cols="30" onChange={handleInputChange}> </textarea>
+                                <div className="contactFooter">
+                                    <div className='sentMessage'>
+                                        {sent ? <span>YOUR MESSAGE HAS BEEN SENT</span> : <span> </span>}
+                                    </div>
+                                    <Link to='mailto:info@ora-arts.com'><h3 className='contact-email'>INFO@ORA-ARTS.COM</h3></Link>
+                                    <button type="submit" className="btnWhite">SEND MESSAGE</button>
+                                </div>
+                            </div>
+                        </form>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="teamTab">
+                            <div className='teamMember'>
+                                <h3>THIBAULT HENRIET</h3>
+                                       <span>PRESIDENT AND FOUNDER</span> 
+                                    </div>
+                            <div className='teamMember'>
+                                <h3>LAURELINE CALASTRENG</h3>
+                                    <span>EXECUTIVE DIRECTOR</span>     
+                                    </div>
+                            <div className='teamMember'>
+                                <h3>RACHEL LEQUESNE</h3>
+                                       <span>HEAD OF DIFFUSION</span> 
+                                    </div>
+                            <div className='teamMember'>
+                                <h3>ARTHUR MORISSET</h3>
+                                       <span>ARTISTIC DIRECTOR / HEAD OF PRODUCTION</span> 
+                                    </div>
+                            <div className='teamMember'>
+                                <h3>CORENTINE PIETTE</h3>
+                                       <span>CTO</span> 
+                                    </div>
+                            <div className='teamMember'>
+                                <Link to='mailto:info@ora-arts.com'><h3 className='contact-email'>INFO@ORA-ARTS.COM</h3></Link>
+                            </div>
+                        </div>
+                    </TabPanel>
+                </Tabs>
             </div>
         </div>
-
     )
 }
