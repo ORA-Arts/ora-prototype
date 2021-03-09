0b0e1c59a4323132ce4a6363d11187aa908b13c2
Louisiane_Berlin <corentine.piette@gmail.com>
2021-03-09 23:08:24 +0100

    status quo

--- client/src/components/CollectorProfile/CollectorProfile.js
+++ client/src/components/CollectorProfile/CollectorProfile.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./CollectorProfile.css";
-import { fetchCollector, addNewCollector, editCollector } from "../../api/service";
+import {
+  fetchCollector,
+  addNewCollector,
+  editCollector,
+} from "../../api/service";
+import ProfileSideBar from "../ProfileSideBar/ProfileSideBar";
 
 const CollectorProfile = (props) => {
   const initialState = {
@@ -24,7 +29,6 @@ const CollectorProfile = (props) => {
     "Often (every couple of months)",
   ];
   const [checkedRel, setCheckedRel] = useState("");
-
 
   const onChange = (event) => {
     const { name, value } = event.target;
@@ -56,49 +60,44 @@ const CollectorProfile = (props) => {
     setIsEditMode(!isEditMode);
   };
 
-
-
-const submitHandler = async () => {
-  const uploadData = new FormData();
-  const dataCopy = data;
-  dataCopy.newsletter = newsletter;
-  dataCopy.budget = Number(dataCopy.budget)
-  for (let key in dataCopy) {
-    uploadData.append(key, dataCopy[key]);
-  }
-  for (let value of uploadData.values()) {
-    console.log(value)
-  }
-
-
-  let resData;
-  if (isEditMode && isCollectorExist) {
-    resData = await editCollector(uploadData);
-  } else {
-    resData = await addNewCollector(uploadData)
-  }
-  setData(resData);
-  setIsEditMode(false);
-  setIsCollectorExist(true);
-  props.changeCollectorName(resData.name)
-};
-
-useEffect(() => {
-  async function fetchData() {
-    await props.setUser(props.user);
-    const resData = await fetchCollector();
-    if (!resData) {
-      setIsCollectorExist(false);
-    } else {
-      setIsCollectorExist(true);
-      setIsEditMode(false);
-      setData(resData);
+  const submitHandler = async () => {
+    const uploadData = new FormData();
+    const dataCopy = data;
+    dataCopy.newsletter = newsletter;
+    dataCopy.budget = Number(dataCopy.budget);
+    for (let key in dataCopy) {
+      uploadData.append(key, dataCopy[key]);
     }
-  }
-  fetchData();
-}, []);
+    for (let value of uploadData.values()) {
+      console.log(value);
+    }
 
+    let resData;
+    if (isEditMode && isCollectorExist) {
+      resData = await editCollector(uploadData);
+    } else {
+      resData = await addNewCollector(uploadData);
+    }
+    setData(resData);
+    setIsEditMode(false);
+    setIsCollectorExist(true);
+    props.changeCollectorName(resData.name);
+  };
 
+  useEffect(() => {
+    async function fetchData() {
+      await props.setUser(props.user);
+      const resData = await fetchCollector();
+      if (!resData) {
+        setIsCollectorExist(false);
+      } else {
+        setIsCollectorExist(true);
+        setIsEditMode(false);
+        setData(resData);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="indexWrap">
@@ -106,148 +105,160 @@ useEffect(() => {
         <div className="collectorHeader">
           <div className="collector-name">
             <h1>{data.firstName}</h1>
-            <button className="btnPrivate">
-              MAKE A PRIVATE SALES REQUEST
-            </button>
+            <button className="btnPrivate">MAKE A PRIVATE SALES REQUEST</button>
           </div>
         </div>
         <hr />
         <div id="formContainer">
-          <div className="personalInformation">
-            <h1>PERSONAL INFORMATION</h1>
-            <div className="btnContainer">
-              <button className="btn-edit" onClick={startEditing}>
-                Edit
-              </button>
-            </div>
-          </div>
-          <hr />
-          <div className="input-block">
-            <div className="input-container-half">
-              <span className="input-label input-lable-right">
-                FIRST NAME/{" "}
-              </span>
-              {isEditMode ? (
-                <input
-                  type="text"
-                  name="firstName"
-                  onChange={onChange}
-                  value={data.firstName}
-                  className="input-no-border"
-                  placeholder="Your First Name"
-                  role="textbox"
-                ></input>
-              ) : (
-                <span>{data.firstName}</span>
-              )}
-            </div>
-            <div className="input-container-half">
-              <span className="input-label input-lable-right">LAST NAME/ </span>
-              {isEditMode ? (
-                <input
-                  type="text"
-                  name="lastName"
-                  onChange={onChange}
-                  value={data.lastName}
-                  className="input-no-border"
-                  placeholder="Your Last Name"
-                ></input>
-              ) : (
-                <span>{data.lastName}</span>
-              )}
-            </div>
-            <div className="input-container-half">
-              {/* should not display even in edit */}
-              <span className="input-label input-lable-right">EMAIL/ </span>
-              {isEditMode ? (
-                <input
-                  type="email"
-                  name="email"
-                  onChange={onChange}
-                  value={data.email}
-                  className="input-no-border"
-                  placeholder="youremail@mail.com"
-                ></input>
-              ) : (
-                <span>{data.email}</span>
-              )}
-            </div>
-            <div className="input-container-half">
-              {/* should not display even in edit */}
-              <span className="input-label input-lable-left">BIRTHDATE/ </span>
-              {isEditMode ? (
-                <input
-                  type="string"
-                  name="birthdate"
-                  onChange={onChange}
-                  value={data.birthdate}
-                  className="input-no-border"
-                  placeholder="YOUR BIRTHDATE"
-                ></input>
-              ) : (
-                <span>{data.email}</span>
-              )}
-            </div>
-            <div className="input-container-full">
-              <span className="input-label">ADDRESS/ </span>
-              {isEditMode ? (
-                <input
-                  type="text"
-                  name="address"
-                  onChange={onChange}
-                  value={data.address}
-                  className="input-no-border"
-                  placeholder="YOUR ADDRESS"
-                ></input>
-              ) : (
-                <span>{data.address}</span>
-              )}
-            </div>
+          <ProfileSideBar content="my-collector-profile" />
+          <div id="formContainerField">
             <div className="personalInformation">
-              <h1>COLLECTOR DATA</h1>
-            </div>
-            <div className="input-block">
-              <div className="collectorType">
-                <span className="inputLabel">COLLECTOR TYPE/ </span>
-                {isEditMode ? (
-                  relCheckboxes.map((item) => (
-                    <div className="inputClear behaviourCheck">
-                      <label key={item.key}>
-                        <Checkbox
-                          name={item.name}
-                          checked={checkedRel === item.name}
-                          onChange={handleCheckboxRel}
-                        />
-                        <span>{item.name}</span>
-                      </label>
-                    </div>
-                  ))
-                ) : (
-                  <span> {data.behaviour} </span>
-                )}
+              <h1 className="title-text" >PERSONAL INFORMATION</h1>
+              <div className="btnContainer">
+                <button className="btn-edit" onClick={startEditing}>
+                  Edit
+                </button>
               </div>
-              <div className="input-container-half budget">
-                {/* should not display even in edit */}
-                <span className="input-label input-lable-left">
-                  I SPEND APPROXIMATELY (k€/Y) ON ART
+            </div>
+            <hr />
+            <div className="input-block">
+              <div className="input-container-half">
+                <span className="input-label input-lable-right">
+                  FIRST NAME/{" "}
                 </span>
                 {isEditMode ? (
                   <input
-                    type="number"
-                    name="budget"
+                    type="text"
+                    name="firstName"
                     onChange={onChange}
-                    value={data.budget}
+                    value={data.firstName}
                     className="input-no-border"
-                    placeholder="5"
+                    placeholder="Your First Name"
+                    role="textbox"
                   ></input>
                 ) : (
-                  <span>{data.email}</span>
+                  <span>{data.firstName}</span>
                 )}
               </div>
-
+              <div className="input-container-half">
+                <span className="input-label input-lable-left">
+                  LAST NAME/{" "}
+                </span>
+                {isEditMode ? (
+                  <input
+                    type="text"
+                    name="lastName"
+                    onChange={onChange}
+                    value={data.lastName}
+                    className="input-no-border"
+                    placeholder="Your Last Name"
+                  ></input>
+                ) : (
+                  <span>{data.lastName}</span>
+                )}
+              </div>
             </div>
-            <div id="selectTopic">
-                <span className="input-label">NEWSLETTER</span>
+            <div className="input-block">
+                <div className="input-container-half">
+                  {/* should not display even in edit */}
+                  <span className="input-label input-lable-right">EMAIL/ </span>
+                  {isEditMode ? (
+                    <input
+                      type="email"
+                      name="email"
+                      onChange={onChange}
+                      value={data.email}
+                      className="input-no-border"
+                      placeholder="youremail@mail.com"
+                    ></input>
+                  ) : (
+                    <span>{data.email}</span>
+                  )}
+                </div>
+                <div className="input-container-half">
+                  {/* should not display even in edit */}
+                  <span className="input-label input-lable-left">
+                    BIRTHDATE/{" "}
+                  </span>
+                  {isEditMode ? (
+                    <input
+                      type="string"
+                      name="birthdate"
+                      onChange={onChange}
+                      value={data.birthdate}
+                      className="input-no-border"
+                      placeholder="YOUR BIRTHDATE"
+                    ></input>
+                  ) : (
+                    <span>{data.email}</span>
+                  )}
+                </div>
+              </div>
+              <div className="input-container-full">
+                <span className="input-label">ADDRESS/ </span>
+                {isEditMode ? (
+                  <input
+                    type="text"
+                    name="address"
+                    onChange={onChange}
+                    value={data.address}
+                    className="input-no-border"
+                    placeholder="YOUR ADDRESS"
+                  ></input>
+                ) : (
+                  <span>{data.address}</span>
+                )}
+              </div>
+              <hr/>
+              <div className="personalInformation">
+                <h1>COLLECTOR DATA</h1>
+              </div>
+              <hr/>
+              <div className="input-block">
+                <div className="collectorType">
+                  <span className="inputLabel">COLLECTOR TYPE/ </span>
+                  {isEditMode ? (
+                    relCheckboxes.map((item) => (
+                      <div className="inputClear behaviourCheck">
+                        <label key={item.key}> 
+                          <Checkbox
+                            name={item.name}
+                            checked={checkedRel === item.name}
+                            onChange={handleCheckboxRel}
+                          />
+                          <span>{item.name}</span>
+                          </label>
+                      </div>
+                    ))
+                  ) : (
+                    <span style={{alignSelf:"flex-start"}}> {data.behaviour} </span>
+                  )}
+                </div>
+                <div>
+                  <div className="input-container-half budget">
+                    {/* should not display even in edit */}
+                    <span id="bugetLabel">
+                      I SPEND APPROXIMATELY (k€/Y) ON ART
+                    </span>
+                    {isEditMode ? (
+                      <input
+                        type="number"
+                        name="budget"
+                        onChange={onChange}
+                        value={data.budget}
+                        className="input-no-border"
+                        placeholder="5"
+                      ></input>
+                    ) : (
+                      <span>{data.budget} k€ per year</span>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div id="newsBool">
+                <span className="input-label">
+                  NEWSLETTER /
+                </span>
                 {isEditMode ? (
                   <>
                     <input
@@ -268,18 +279,23 @@ useEffect(() => {
                     <label htmlFor="no">NO</label>
                   </>
                 ) : newsletter ? (
-                  "YES"
+                  "   YES"
                 ) : (
-                  "NO"
+                  "   NO"
                 )}
+              </div>
+              {isEditMode ? (
+                <button
+                  id="saveButton"
+                  onClick={submitHandler}
+                >
+                  SAVE CHANGES
+                </button>
+              ) : null}
             </div>
-            {isEditMode ? 
-            <button className="btnSave save-change space-top" onClick={submitHandler}>SAVE CHANGES</button>
-            : null}
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
